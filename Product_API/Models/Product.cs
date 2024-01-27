1ec04da7961e1efb7f7d2961fffcaa98ef82f03f
EbrahemElbetar1 <132098790+EbrahemElbetar1@users.noreply.github.com>
2024-01-27 12:41:25 +0200

    V_1 Models

---
+++ Product_API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product_API.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
